d438e04e53d5794edb7b66987e27ad3f82c3f8e3
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-16 15:12:16 -0300

    Desafio Parte 2: XStreamDataFormat e split

--- src/main/java/br/com/caelum/camel/desafio/RotaHttpPollingNegociacoes.java
+++ src/main/java/br/com/caelum/camel/desafio/RotaHttpPollingNegociacoes.java
@@ -1,13 +1,17 @@
 package br.com.caelum.camel.desafio;
 
+import com.thoughtworks.xstream.XStream;
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class Desafio1 {
+public class RotaHttpPollingNegociacoes {
 
     public static void main(String[] args) throws Exception {
+
+        final XStream xstream = new XStream();
+        xstream.alias("negociacao", Negociacao.class);
 
         CamelContext context = new DefaultCamelContext();
         context.addRoutes(new RouteBuilder() {
@@ -18,9 +22,12 @@ public class Desafio1 {
                 from("timer://negociacoes?fixedRate=true&delay=1s&period=360s").
                         to("http4://argentumws-spring.herokuapp.com/negociacoes").
                         convertBodyTo(String.class).
+                        unmarshal(new XStreamDataFormat(xstream)).
+                        split(body()).
                         log("${body}").
-                        setHeader(Exchange.FILE_NAME, constant("negociacoes.xml")).
-                to("file:saida");
+                end(); //só deixa explícito que é o fim da rota
+//                        setHeader(Exchange.FILE_NAME, constant("negociacoes.xml")).
+//                to("file:saida");
             }
         });
 
