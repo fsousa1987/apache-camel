74439be905bce6d929f18af013f794751c16e4ae
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-17 08:15:14 -0300

    Melhor legibilidade com sub-rotas

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -17,6 +17,13 @@ public class RotaPedidos {
 			public void configure() {
 
 				from("file:pedidos?delay=5s&noop=true").
+				routeId("rota-pedidos").
+				multicast().
+					to("direct:soap").
+					to("direct:http");
+
+				from("direct:http").
+						routeId("rota-http").
 						setProperty("pedidoId", xpath("/pedido/id/text()")).
 						setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
 						split()
@@ -30,7 +37,12 @@ public class RotaPedidos {
 						setHeader(Exchange.HTTP_QUERY,
 								simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}" +
 										"&clienteId=${property.clienteId}")).
-						to("http4://localhost:8080/webservices/ebook/item");
+				to("http4://localhost:8080/webservices/ebook/item");
+
+				from("direct:soap").
+						routeId("rota-soap").
+						setBody(constant("<envelope>teste</envelope>")).
+				to("mock:soap");
 			}
 		});
 
